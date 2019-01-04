e3cd442c0e32c23a18b4247ad241f20e98484519
Tony StarrK <tony.kino.3@gmail.com>
2019-01-04 20:33:15 +0100

    Forgetting users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
       remember user
       redirect_to user
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
