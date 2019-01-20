7d8841adca633536f7ac97278105bda74186c700
Tony StarrK <tony.kino.3@gmail.com>
2019-01-20 14:27:29 +0100

    Administrative users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
+User.create!(name: "Tony",
+             email: "tony@admin.com",
+             password: "password",
+             password_confirmation: "password",
+             admin: true)
+
 User.create!(name: "Example User",
              email: "example@railstutorial.org",
              password: "foobar",
              password_confirmation: "foobar")
 
-99.times do |n|
+498.times do |n|
   name  = Faker::Name.name
   email = "example-#{n + 1}@railstutorial.org"
   password = "password"
